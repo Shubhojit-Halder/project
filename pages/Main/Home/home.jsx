cf4597eafea7e23210898d59f51d797da1c61225
ani <subhradeeppal30@gmail.com>
2022-07-05 02:50:08 +0530

    issue with last child fixed

--- pages/Main/Home/home.jsx
+++ pages/Main/Home/home.jsx
@@ -26,80 +26,82 @@ const Home = () => {
 	return (
 		<div>
 			<Navbar />
-
-			<div className={style.main} id="main">
-				<div className={style.gridMain}>
-					<Box sx={{ flexGrow: 1 }}>
-						<Grid container spacing={2}>
-							<Grid item xs={0} sm={0} md={0} lg={3}>
-								<Paper elevation={3} className={style.section1} />
-								<Box mt={3}>
-									<Paper
-										elevation={3}
-										className={style.section6}
-										style={{ minHeight: "250px" }}
-									>
-										<Profile />
-									</Paper>
-								</Box>
-
-								<Paper
-									elevation={3}
-									id="section4"
-									className={style.section4}
-									style={{ height: "320px" }}
-								>
-									<MenuList></MenuList>
-								</Paper>
-								{div ? (
-									<>
-										<div className={style.hold}>
-											<Paper
-												elevation={3}
-												className={style.section7}
-												style={{ height: "320px" }}
-											>
-												<MenuList></MenuList>
-											</Paper>
-										</div>
-									</>
-								) : (
-									<></>
-								)}
-							</Grid>
-							<Grid item xs={12} sm={11} md={7} lg={6}>
-								<div className={style.postContainer}>
-									<div className={style.containerSection2}>
+			<div className={style.sobarbap}>
+				<div className={style.main} id="main">
+					<div className={style.gridMain}>
+						<Box sx={{ flexGrow: 1 }}>
+							<Grid container spacing={2}>
+								<Grid item xs={0} sm={0} md={0} lg={3}>
+									<Paper elevation={3} className={style.section1} />
+									<Box mt={3}>
 										<Paper
 											elevation={3}
-											style={{
-												height: "130px",
-												marginBottom: "30px",
-											}}
-											className={style.section2c}
+											className={style.section6}
+											style={{ minHeight: "250px" }}
 										>
-											<Post />
+											<Profile />
 										</Paper>
-									</div>
-									<ContentContainer />
-								</div>
-							</Grid>
-							<Grid item xs={0} sm={0} md={4} lg={3}>
-								<div className={style.containerSection34}>
-									<Paper elevation={3} className={style.section3} />
+									</Box>
 
-									<Paper elevation={3} className={style.section5} />
+									<Paper
+										elevation={3}
+										id="section4"
+										className={style.section4}
+										style={{ height: "320px" }}
+									>
+										<MenuList></MenuList>
+									</Paper>
 									{div ? (
-										<div className={style.hold2}>
-											<Paper elevation={3} className={style.section5} />
-										</div>
+										<>
+											<div className={style.hold}>
+												<Paper
+													elevation={3}
+													className={style.section7}
+													style={{ height: "320px" }}
+												>
+													<MenuList></MenuList>
+												</Paper>
+											</div>
+										</>
 									) : (
 										<></>
 									)}
-								</div>
+								</Grid>
+								<Grid item xs={12} sm={11} md={7} lg={6}>
+									<div className={style.postContainer}>
+										<div className={style.containerSection2}>
+											<Paper
+												elevation={3}
+												style={{
+													height: "130px",
+													marginBottom: "30px",
+												}}
+												className={style.section2c}
+											>
+												<Post />
+											</Paper>
+										</div>
+										<ContentContainer />
+										<div className={style.gap}></div>
+									</div>
+								</Grid>
+								<Grid item xs={0} sm={0} md={4} lg={3}>
+									<div className={style.containerSection34}>
+										<Paper elevation={3} className={style.section3} />
+
+										<Paper elevation={3} className={style.section5} />
+										{div ? (
+											<div className={style.hold2}>
+												<Paper elevation={3} className={style.section5} />
+											</div>
+										) : (
+											<></>
+										)}
+									</div>
+								</Grid>
 							</Grid>
-						</Grid>
-					</Box>
+						</Box>
+					</div>
 				</div>
 			</div>
 		</div>
