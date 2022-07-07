2c1576291a1022e59943137a498d41d53f72e07d
ani <subhradeeppal30@gmail.com>
2022-07-07 12:45:26 +0530

    follow section

--- pages/Main/Home/contentContainer.jsx
+++ pages/Main/Home/contentContainer.jsx
@@ -7,32 +7,32 @@ const ContentContainer = () => {
 	return (
 		<>
 			<div className={style.containerSection2}>
-				<Paper elevation={3} className={style.section2}>
+				<Paper elevation={1} className={style.section2}>
 					<Contents />
 				</Paper>
 			</div>
 			<div className={style.containerSection2}>
-				<Paper elevation={3} className={style.section2}>
+				<Paper elevation={1} className={style.section2}>
 					<SharedContent />
 				</Paper>
 			</div>
 			<div className={style.containerSection2}>
-				<Paper elevation={3} className={style.section2}>
+				<Paper elevation={1} className={style.section2}>
 					<Contents />
 				</Paper>
 			</div>
 			<div className={style.containerSection2}>
-				<Paper elevation={3} className={style.section2}>
+				<Paper elevation={1} className={style.section2}>
 					<Contents />
 				</Paper>
 			</div>
 			<div className={style.containerSection2}>
-				<Paper elevation={3} className={style.section2}>
+				<Paper elevation={1} className={style.section2}>
 					<Contents />
 				</Paper>
 			</div>
 			<div className={style.containerSection2}>
-				<Paper elevation={3} className={style.section2}>
+				<Paper elevation={1} className={style.section2}>
 					<Contents />
 				</Paper>
 			</div>
